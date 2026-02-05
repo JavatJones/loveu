dc2d2d4ee1a2a4484e48f88ae176ea7aa2f430d6
JavatJones <javiervillagomez96@gmail.com>
2026-02-05 13:18:15 -0600

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
